70e0fe6db7c8d3684c5be382a1464720749e7362
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-05-19 15:02:35 +0200

    updated the scale testing functionality

--- test_OnlineMeanVar.py
+++ test_OnlineMeanVar.py
@@ -1,11 +1,16 @@
 
-import numpy as np;
+import numpy as np
+import math
 
 import OnlineMeanVar as omv
 
 def test_LocShift():
+    # create random data with strong differences in mean and "local" variance, to make life harder
+    # for numeric accuracy. We do this by distorting the mean of the first 10 nucleotides.
     l = np.random.randint(10000,1000000)
-    xs = np.random.random(l)
+    d = np.random.exponential(scale = 20)
+    xs = np.append( np.random.gumbel(loc=d, scale = 10, size = 10)
+                  , np.random.gumbel(loc=0, scale = 10, size=l) )
     ls = omv.LocShift()
     ys = np.array_split(xs, l / 10)
     print("size xs: {}, num splits: {}".format(xs.size, len(ys)))
@@ -14,7 +19,9 @@ def test_LocShift():
     mean = np.mean(xs)
     var = np.var(xs)
     kmean, kvar = ls.meanVar()
-    print("mean: {}, var: {}, k-mean: {}, k-var: {}, diff-mean: {}, diff-var: {}".format(mean, var, kmean, kvar, mean-kmean, var-kvar))
-    assert (abs(mean-kmean) < var)
-    assert (abs(var-kvar) < 0.001)
+    meankmean = math.log(mean) - math.log(kmean)
+    varkvar = math.log(var) - math.log(kvar)
+    print("mean: {}, var: {}, k-mean: {}, k-var: {}, loc (first 10): {}, mean/kmean: {}, var/kvar: {}".format(mean, var, kmean, kvar, d, meankmean, varkvar))
+    assert (meankmean < 0.01)
+    assert (varkvar < 0.1)
 
